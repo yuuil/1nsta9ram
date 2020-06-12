d99750132a6c595e3b728cde5612d94c5110bb6a
yuuil <yeyeah_1@daum.net>
2020-06-12 21:25:58 +0900

    🔧 [FIXED] After confirm secret code, delete that secret code

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -3,11 +3,17 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    confirmSecret: async (_, args, {request}) => {
+    confirmSecret: async (_, args, { request }) => {
       console.log(request);
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: {
+            loginSecret: "",
+          },
+        });
         return generateToken(user.id);
       } else {
         throw Error("Wrong email/secret combination");
