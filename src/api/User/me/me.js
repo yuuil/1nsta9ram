a50ccebbb2d6bb8ad2b63585038d1aaa1a0d557f
yuuil <yeyeah_1@daum.net>
2020-06-14 18:49:01 +0900

    📝 [ADD] isFollowing / isSelf

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -1,12 +1,16 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { USER_FRAGMENT } from "../../../fragment";
 
 export default {
   Query: {
-    me: (_, __, { request, isAuthenticated }) => {
+    me: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      return prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
+      const userProfile = await prisma.user({id: user.id});
+      const posts = await prisma.user({id: user.id}).posts();
+      return {
+        user: userProfile,
+        posts
+      }
     },
   },
 };
--- src/fragment.js
+++ src/fragment.js
@@ -1,17 +1,3 @@
-export const USER_FRAGMENT = `
-  fragment UserParts on User {
-    id
-    username
-    email
-    firstName
-    bio
-    posts {
-      id
-      caption
-    }
-  }
-`;
-
 export const COMMENT_FRAGMENT = `
   fragment CommentParts on Comment {
     id
