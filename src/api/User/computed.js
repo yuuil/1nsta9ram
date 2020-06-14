574bebf2b1446ddcb140abd20739792e5022aefb
yuuil <yeyeah_1@daum.net>
2020-06-14 19:24:05 +0900

    📝 [ADD] isLiked Post

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -22,4 +22,20 @@ export default {
       return user.id === parentId;
     },
   },
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: { id: user.id },
+          },
+          {
+            post: { id },
+          },
+        ],
+      });
+    },
+  },
 };
