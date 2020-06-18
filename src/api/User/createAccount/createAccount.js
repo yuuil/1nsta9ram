f7160e7345ff346643940d9e5d252c2ec1a74719
yuuil <yeyeah_1@daum.net>
2020-06-18 18:56:54 +0900

    🔧 [FIXED] createAccount Mutation

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,18 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
-        username,
-        email,
-        firstName,
-        lastName,
-        bio,
-      });
-      return user;
+      try {
+        await prisma.createUser({
+          username,
+          email,
+          firstName,
+          lastName,
+          bio,
+        });
+        return true;
+      } catch {
+        return false;
+      }
     },
   },
 };
