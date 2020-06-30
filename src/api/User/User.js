a1ac6e61f6a0485dc920dc48f05db62ec1dfa306
yuuil <yeyeah_1@daum.net>
2020-07-01 02:18:07 +0900

    🔧 [FIXED] API for User

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -1,29 +1,30 @@
 import { prisma } from "../../../generated/prisma-client";
 
 export default {
-  posts: ({ id }) => prisma.user({ id }).posts(),
-  following: ({ id }) => prisma.user({ id }).following(),
-  followers: ({ id }) => prisma.user({ id }).followers(),
-  likes: ({ id }) => prisma.user({ id }).likes(),
-  comments: ({ id }) => prisma.user({ id }).comments(),
-  rooms: ({ id }) => prisma.user({ id }).rooms(),
-  postsCount: ({ id }) =>
-    prisma
-      .postsConnection({ where: { user: { id } } })
-      .aggregate()
-      .count(),
-  followingCount: ({ id }) =>
-    prisma
-      .usersConnection({ where: { followers_some: { id } } })
-      .aggregate()
-      .count(),
-  followersCount: ({ id }) =>
-    prisma
-      .usersConnection({ where: { following_none: { id } } })
-      .aggregate()
-      .count(),
   User: {
+    posts: ({ id }) => prisma.user({ id }).posts(),
+    following: ({ id }) => prisma.user({ id }).following(),
+    followers: ({ id }) => prisma.user({ id }).followers(),
+    likes: ({ id }) => prisma.user({ id }).likes(),
+    comments: ({ id }) => prisma.user({ id }).comments(),
+    rooms: ({ id }) => prisma.user({ id }).rooms(),
+    postsCount: ({ id }) =>
+      prisma
+        .postsConnection({ where: { user: { id } } })
+        .aggregate()
+        .count(),
+    followingCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { followers_some: { id } } })
+        .aggregate()
+        .count(),
+    followersCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { following_none: { id } } })
+        .aggregate()
+        .count(),
     fullName: (parent) => {
+      if (!parent.firstName && !parent.lastName) return "";
       return `${parent.firstName} ${parent.lastName}`;
     },
     isFollowing: async (parent, _, { request }) => {
