ee2d2754bbcbca7e48b95e07f6af4c0c4007f31e
yuuil <yeyeah_1@daum.net>
2020-07-15 18:27:52 +0900

    🔧 [FIXED] Upload Photo to S3

--- src/upload.js
+++ src/upload.js
@@ -1,12 +1,31 @@
-const multer = require("multer");
+import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-const upload = multer({ dest: "uploads/" });
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_API_ID,
+  secretAccessKey: process.env.S3_API_SECRET_KEY,
+  region: "ap-northeast-2",
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "1nsta9ram",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
 
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const {
-    file: { path },
+    file: { location },
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
