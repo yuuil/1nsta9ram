e65f41af06e5b5d926d0cde052fabe5379539eee
yuuil <yeyeah_1@daum.net>
2020-07-15 17:58:56 +0900

    FIXED] API for Upload Photo with REST

---
+++ src/upload.js
@@ -0,0 +1,12 @@
+const multer = require("multer");
+
+const upload = multer({ dest: "uploads/" });
+
+export const uploadMiddleware = upload.single("file");
+
+export const uploadController = (req, res) => {
+  const {
+    file: { path },
+  } = req;
+  res.json({ path });
+};
